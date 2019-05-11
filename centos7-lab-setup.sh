4d182262d636845c10a1406612ff4197e9a45eb1
Justin Oeder <jcoeder@gmail.com>
2019-05-11 15:39:57 -0400

    Update centos7-lab-setup.sh

--- centos7-lab-setup.sh
+++ centos7-lab-setup.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+# Install EPEL
+# Update
+# Install docker, traceroute, telnet, iperf, wget
+# Remove firewalld
+# Set SELINUX to permissive
+# Reboot
+
 yum update -y && yum install epel-release -y && yum install docker traceroute telnet iperf wget -y && yum remove firewalld -y && systemctl enable docker && systemctl start docker
 sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config && cat /etc/selinux/config && reboot
