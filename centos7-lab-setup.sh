b0409e9ad445905603363abd29859cf1b4c6cdd7
Justin Oeder <jcoeder@gmail.com>
2019-05-12 23:53:34 -0400

    added git

--- centos7-lab-setup.sh
+++ centos7-lab-setup.sh
@@ -7,5 +7,5 @@
 # Set SELINUX to permissive
 # Reboot
 
-yum update -y && yum install epel-release -y && yum install docker traceroute telnet iperf wget -y && yum remove firewalld -y && systemctl enable docker && systemctl start docker
+yum update -y && yum install epel-release -y && yum install docker traceroute telnet iperf wget git -y && yum remove firewalld -y && systemctl enable docker && systemctl start docker
 sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config && cat /etc/selinux/config && reboot
