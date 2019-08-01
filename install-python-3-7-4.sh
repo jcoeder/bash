d206c245c1f0163de8510c8efc4a4a54a5274cd5
Justin Oeder <jcoeder@gmail.com>
2019-07-31 23:15:19 -0400

    Update install-python-3-7-4.sh

--- install-python-3-7-4.sh
+++ install-python-3-7-4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install gcc openssl-devel bzip2-devel libffi-devel
+sudo yum install gcc openssl-devel bzip2-devel libffi-devel -y
 sudo wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
 sudo tar xzf Python-3.7.4.tgz
 cd Python-3.7.4
