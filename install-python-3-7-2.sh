fe89f6c0963247a0ee385ae3b1f4796a7c11f907
Justin Oeder <jcoeder@gmail.com>
2019-07-31 23:15:07 -0400

    Update install-python-3-7-2.sh

--- install-python-3-7-2.sh
+++ install-python-3-7-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install gcc openssl-devel bzip2-devel
+sudo yum install gcc openssl-devel bzip2-devel -y
 sudo wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 sudo tar xzf Python-3.7.2.tgz
 cd Python-3.7.2
