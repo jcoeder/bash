71e17afb6f08cf57232c3b24bc734f88050385da
Justin Oeder <jcoeder@gmail.com>
2019-07-31 23:15:34 -0400

    Update install-python-3-8-0b3.sh

--- install-python-3-8-0b3.sh
+++ install-python-3-8-0b3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install gcc openssl-devel bzip2-devel
+sudo yum install gcc openssl-devel bzip2-devel -y
 sudo wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0b3.tgz
 sudo tar xzf Python-3.8.0b3.tgz
 cd Python-3.8.0b3
