9df3c5241e187cad478275bca30bf03fa85062db
Justin Oeder <jcoeder@gmail.com>
2019-07-31 23:08:49 -0400

    Create install-python-3-7-4.sh

---
+++ install-python-3-7-4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo yum install gcc openssl-devel bzip2-devel
+sudo wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
+sudo tar xzf Python-3.7.4.tgz
+cd Python-3.7.4
+sudo ./configure --enable-optimizations
+sudo make altinstall
