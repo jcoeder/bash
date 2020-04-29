8b5bb6fb0ead545f97c5e3a3035b73ce779cff80
Justin Oeder <jcoeder@gmail.com>
2020-04-28 22:39:56 -0400

    Create install-python-3-7-3.sh

---
+++ install-python-3-7-3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum install gcc openssl-devel bzip2-devel -y
+sudo wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
+sudo tar xzf Python-3.7.3.tgz
+cd Python-3.7.3
+sudo ./configure --enable-optimizations
+sudo make altinstall
+pip3.7 install virtualenv --user
