a87cf0510efa180feeaa21106f127e481de7c53f
Justin Oeder <jcoeder@gmail.com>
2020-04-28 23:09:05 -0400

    Update install-python-3-7-3.sh

--- install-python-3-7-3.sh
+++ install-python-3-7-3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install gcc openssl-devel bzip2-devel libffi libffi-devel -y
+sudo yum install gcc openssl-devel bzip2-devel libffi libffi-devel sqlite sqlite-devel-y
 sudo wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
 sudo tar xzf Python-3.7.3.tgz
 cd Python-3.7.3
